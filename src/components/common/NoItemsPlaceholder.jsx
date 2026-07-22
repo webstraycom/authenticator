d5abedad195f1c69acf603347eda332543a45c35
KolyaFedorenko <kolyafedorenko645@gmail.com>
2026-07-22 13:52:26 +0700

    fix(no-items-placeholder): update documentation link

--- src/components/common/NoItemsPlaceholder.jsx
+++ src/components/common/NoItemsPlaceholder.jsx
@@ -34,7 +34,7 @@ export const NoItemsPlaceholder = ({ onAdd, onImport, options }) => {
         </PopoverTrigger>
         <PopoverContent className="text-muted-foreground w-72 gap-0 text-sm">
           You can view the WebStray Authenticator documentation at{' '}
-          <p className="underline underline-offset-4">https://webstray.com/authenticator/docs</p>
+          <p className="underline underline-offset-4">https://webstray.com/docs/authenticator</p>
         </PopoverContent>
       </Popover>
     </Empty>
