681d028ef2e46af8f1f3aa517966e2fe671bad44
KolyaFedorenko <kolyafedorenko645@gmail.com>
2026-08-09 00:32:55 +0700

    refactor(common): replace LoaderIcon with Spinner component

--- src/components/common/app-loading-placeholder.jsx
+++ src/components/common/app-loading-placeholder.jsx
@@ -1,12 +1,12 @@
-import { LoaderIcon } from 'lucide-react';
 import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@ui/empty';
+import { Spinner } from '@ui/spinner';
 
 export const AppLoadingPlaceholder = () => {
   return (
     <Empty className="animate-in fade-in w-full duration-250">
       <EmptyHeader>
         <EmptyMedia className="bg-accent size-8" variant="icon">
-          <LoaderIcon className="size-4 animate-spin" />
+          <Spinner />
         </EmptyMedia>
         <EmptyTitle className="text-sm">Authenticator is loading</EmptyTitle>
         <EmptyDescription>
