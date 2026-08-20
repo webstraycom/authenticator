3df2f991279bc5942ae458779b431553903fcc57
KolyaFedorenko <kolyafedorenko645@gmail.com>
2026-08-20 09:45:24 +0700

    feat(header): add search button and rework layout

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,14 @@
-import { useUIStore } from '@store';
-import { PanelLeft } from 'lucide-react';
+import { PanelLeft, SearchIcon } from 'lucide-react';
 import { Button } from '@ui/button';
+import { Kbd, KbdGroup } from '@ui/kbd';
+import { Separator } from '@ui/separator';
 import { useSidebar } from '@ui/sidebar';
+import { useUIStore } from '@store';
 
 export const Header = () => {
   const { toggleSidebar } = useSidebar();
   const screen = useUIStore((state) => state.currentScreen);
+  const openCommandPalette = useUIStore((state) => state.openCommandPalette);
 
   const screenTitles = {
     passwords: 'Passwords',
@@ -15,19 +18,29 @@ export const Header = () => {
   };
 
   return (
-    <div className="flex h-12 shrink-0 items-center gap-1.5 border-b px-6.5">
-      <Button
-        variant="ghost"
-        type="button"
-        onClick={toggleSidebar}
-        className="text-muted-foreground size-7 rounded-md p-1.5 transition duration-150 hover:text-current"
-      >
-        <PanelLeft className="size-4" strokeWidth={2} />
-      </Button>
-
-      <span className="text-muted-foreground text-xs font-medium uppercase">
-        {screenTitles[screen]}
-      </span>
+    <div className="flex h-12 w-full shrink-0 items-center justify-center border-b px-6.5">
+      <div className="flex w-full max-w-xl items-center justify-between">
+        <div className="text-muted-foreground flex items-center gap-1.5">
+          <Button variant="ghost" size="icon-sm" onClick={toggleSidebar}>
+            <PanelLeft className="size-4" strokeWidth={2} />
+          </Button>
+          <Separator orientation="vertical" className="mr-1.5 size-4 !self-center" />
+          <span className="text-sm font-medium">{screenTitles[screen]}</span>
+        </div>
+        <Button
+          variant="ghost"
+          onClick={openCommandPalette}
+          className="group text-muted-foreground gap-1.5 px-1.5"
+        >
+          <SearchIcon />
+          Search
+          <Separator orientation="vertical" className="mx-1.5 size-4 !self-center" />
+          <KbdGroup className="dark:group-hover:[&_kbd]:bg-muted group-hover:[&_kbd]:bg-foreground/5 gap-0.75 [&_kbd]:transition-all">
+            <Kbd>Ctrl</Kbd>
+            <Kbd>K</Kbd>
+          </KbdGroup>
+        </Button>
+      </div>
     </div>
   );
 };
